4b32348d9a906b7fcbf6bbfae6c9f34218643c99
Yash Sen <yashsen0402@gmail.com>
2024-09-25 17:39:40 +0530

    Includes Created

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home Categories | Ruper</title>
+	<link rel="shortcut icon" type="image/x-icon" href="media/favicon.png">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/bootstrap/css/bootstrap.min.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/feather-font/css/iconfont.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/icomoon-font/css/icomoon.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/font-awesome/css/font-awesome.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/wpbingofont/css/wpbingofont.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/elegant-icons/css/elegant.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/slick/css/slick.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/slick/css/slick-theme.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/mmenu/css/mmenu.min.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/css/app.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/css/responsive.css') ?>" type="text/css">
+	<link rel="stylesheet" href="<?= urlOf('assets/libs/slider/css/jslider.css') ?>">
+	<link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@100;200;300;400;500;600;700&amp;display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=EB+Garamond:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&amp;display=swap" rel="stylesheet">
+</head>
+<!-- <div class="page-preloader">
+	    	<div class="loader">
+	    		<div></div>
+	    		<div></div>
+	    	</div>
+	    </div> -->
---
+++ includes/navbar.php
@@ -0,0 +1,171 @@
+<header id="site-header" class="site-header header-v1">
+				<div class="header-mobile">
+					<div class="section-padding">
+						<div class="section-container">
+							<div class="row">
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3 header-left">
+									<div class="navbar-header">
+										<button type="button" id="show-megamenu" class="navbar-toggle"></button>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6 header-center">
+									<div class="site-logo">
+										<a href="index-2.html">
+											<img width="400" height="79" src="<?= urlOf('assets/media/logo.png') ?>" alt="Ruper – Furniture HTML Theme" />
+										</a>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3 header-right">
+									<!-- Wishlist -->
+									<div class="wishlist-box">
+												<a href="<?= urlOf('pages/wishlist.php') ?>"><i class="icon-heart"></i></a>
+												<span class="count-wishlist">1</span>
+											</div>
+									<div class="ruper-topcart dropdown">
+										<div class="dropdown mini-cart top-cart">
+											<div class="remove-cart-shadow"></div>
+											<a class="dropdown-toggle cart-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												<div class="icons-cart"><i class="icon-large-paper-bag"></i><span class="cart-count">2</span></div>
+											</a>
+											<div class="dropdown-menu cart-popup">
+												<div class="cart-empty-wrap">
+													<ul class="cart-list">
+														<li class="empty">
+															<span>No products in the cart.</span>
+															<a class="go-shop" href="<?= urlOf('pages/shop.php') ?>">GO TO SHOP<i aria-hidden="true" class="arrow_right"></i></a>
+														</li>
+													</ul>
+												</div>
+												<div class="cart-list-wrap">
+													<ul class="cart-list ">
+														<li class="mini-cart-item">
+															<a href="#" class="remove" title="Remove this item"><i class="icon_close"></i></a>
+															<a href="shop-details.html" class="product-image"><img width="600" height="600" src="<?= urlOf('assets/media/product/3.jpg') ?>" alt=""></a>
+															<a href="shop-details.html" class="product-name">Chair Oak Matt Lacquered</a>		
+															<div class="quantity">Qty: 1</div>
+															<div class="price">$150.00</div>
+														</li>
+													</ul>
+													<div class="total-cart">
+														<div class="title-total">Total: </div>
+														<div class="total-price"><span>$100.00</span></div>
+													</div>
+													<div class="free-ship">
+														<div class="total-percent"><div class="percent" style="width:20%"></div></div>
+													</div>
+													<div class="buttons">
+														<a href="<?= urlOf('pages/cart.php') ?>" class="button btn view-cart btn-primary">View cart</a>
+														<a href="<?= urlOf('pages/checkout.php') ?>" class="button btn checkout btn-default">Check out</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="header-mobile-fixed">
+						<!-- Shop -->
+						<div class="shop-page">
+							<a href="shop-grid-left.html"><i class="wpb-icon-shop"></i></a>
+						</div>
+						<!-- Wishlist -->
+						<div class="wishlist-box">
+							<a href="shop-wishlist.html"><i class="wpb-icon-heart"></i></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="header-desktop">
+					<div class="header-wrapper">
+						<div class="section-padding">
+							<div class="section-container p-l-r">
+								<div class="row">
+									<div class="col-xl-3 col-lg-2 col-md-12 col-sm-12 col-12 header-left">
+										<div class="site-logo">
+											<a href="index-2.html">
+												<img width="400" height="79" src="<?= urlOf('assets/media/logo.png') ?>" alt="Ruper – Furniture HTML Theme" />
+											</a>
+										</div>
+									</div>
+
+									<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 text-center header-center">
+										<div class="site-navigation">
+											<nav id="main-navigation">
+												<ul id="menu-main-menu" class="menu">
+													<li class="level-0 menu-item">
+														<a href="<?= urlOf('index.php') ?>"><span class="menu-item-text">Home</span></a>
+													</li>
+													<li class="level-0 menu-item">
+														<a href="<?= urlOf('pages/shop.php') ?>"><span class="menu-item-text">Shop</span></a>
+													</li>
+													<li class="level-0 menu-item">
+														<a href="<?= urlOf('pages/about.php') ?>"><span class="menu-item-text">About</span></a>
+													</li>
+													<li class="level-0 menu-item">
+														<a href="<?= urlOf('pages/contact.php') ?>"><span class="menu-item-text">Contact</span></a>
+													</li>
+												</ul>
+											</nav>
+										</div>
+									</div>
+
+									<div class="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-12 header-right">
+										<div class="header-page-link">
+											<!-- Wishlist -->
+											<div class="wishlist-box">
+												<a href="<?= urlOf('pages/wishlist.php') ?>"><i class="icon-heart"></i></a>
+												<span class="count-wishlist">1</span>
+											</div>
+											
+											<!-- Cart -->
+											<div class="ruper-topcart dropdown light">
+												<div class="dropdown mini-cart top-cart">
+													<div class="remove-cart-shadow"></div>
+													<a class="dropdown-toggle cart-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+														<div class="icons-cart"><i class="icon-large-paper-bag"></i><span class="cart-count">2</span></div>
+													</a>
+													<div class="dropdown-menu cart-popup">
+														<div class="cart-empty-wrap">
+															<ul class="cart-list">
+																<li class="empty">
+																	<span>No products in the cart.</span>
+																	<a class="go-shop" href="shop-grid-left.html">GO TO SHOP<i aria-hidden="true" class="arrow_right"></i></a>
+																</li>
+															</ul>
+														</div>
+														<div class="cart-list-wrap">
+															<ul class="cart-list ">
+																<li class="mini-cart-item">
+																	<a href="#" class="remove" title="Remove this item"><i class="icon_close"></i></a>
+																	<a href="shop-details.html" class="product-image"><img width="600" height="600" src="media/product/3.jpg" alt=""></a>
+																	<a href="shop-details.html" class="product-name">Chair Oak Matt Lacquered</a>		
+																	<div class="quantity">Qty: 1</div>
+																	<div class="price">$150.00</div>
+																</li>
+															</ul>
+															<div class="total-cart">
+																<div class="title-total">Total: </div>
+																<div class="total-price"><span>$100.00</span></div>
+															</div>
+															<div class="free-ship">
+																<div class="total-percent"><div class="percent" style="width:20%"></div></div>
+															</div>
+															<div class="buttons">
+																<a href="<?= urlOf('pages/cart.php') ?>" class="button btn view-cart btn-primary">View cart</a>
+																<a href="<?= urlOf('pages/checkout.php') ?>" class="button btn checkout btn-default">Check out</a>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
